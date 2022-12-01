e22c1e6e3895ab07761cc8cb723bb9e00c4a1918
Christoph Hornung <christoph.hornung@crosberg.de>
2022-12-01 11:58:50 +0100

    Day1

---
+++ EmbeddedResource.g.cs
@@ -0,0 +1,7 @@
+namespace Chacode2022
+{
+    public static class EmbeddedResources
+    {
+        public static string Day1_txt = "Chacode2022.Day1.txt";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+
+namespace Chacode2022
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			new Day1().Solve();
+		}
+	}
+
+	internal class Day1
+	{
+		public void Solve()
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+
+			using Stream stream = assembly.GetManifestResourceStream(EmbeddedResources.Day1_txt)!;
+			using StreamReader reader = new StreamReader(stream);
+
+			List<Elf> elves = new();
+			int total = 0;
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine()!;
+				if (string.IsNullOrEmpty(line))
+				{
+					elves.Add(new Elf(total));
+					total = 0;
+				}
+				else
+				{
+					total += int.Parse(line);
+				}
+			}
+
+			Console.WriteLine(
+				$"Day 1: {elves.MaxBy(e => e.Calories)!.Calories}:{elves.OrderByDescending(e => e.Calories).Take(3).Sum(e => e.Calories)}");
+		}
+
+		private record Elf(int Calories)
+		{
+		}
+	}
+}
